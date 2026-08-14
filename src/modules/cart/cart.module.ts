6947f5749e5a8cdcb3dc5dc687270f5333b0ca8b
Giabao11082005 <hoanggiabao110820055@gmail.com>
2026-08-14 16:49:37 +0700

    fix(guard): BUG-003 - accept permission for guard

--- src/modules/cart/cart.module.ts
+++ src/modules/cart/cart.module.ts
@@ -5,9 +5,13 @@ import { CartItem } from './entities/cart-item.entity';
 import { ProductVariant } from '../product-variant/entities/product-variant.entity';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cart, CartItem, ProductVariant])],
+  imports: [
+    TypeOrmModule.forFeature([Cart, CartItem, ProductVariant]),
+    UsersModule,
+  ],
   controllers: [CartController],
   providers: [CartService],
   exports: [CartService],
--- src/modules/product-variant/product-variant.module.ts
+++ src/modules/product-variant/product-variant.module.ts
@@ -7,9 +7,10 @@ import { ProductVariantService } from './product-variant.service';
 import { ProductVariant } from './entities/product-variant.entity';
 import { Product } from '../products/entities/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductVariant, Product])],
+  imports: [TypeOrmModule.forFeature([ProductVariant, Product]), UsersModule],
   controllers: [ProductVariantController, VariantController],
   providers: [ProductVariantService],
   exports: [ProductVariantService],
