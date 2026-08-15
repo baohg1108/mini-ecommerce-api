620b27802818e513108d009d5147906c1f23d142
nphuowng205 <phuongww111@gmail.com>
2026-08-15 11:56:28 +0700

    feat(cart): BE-029 - validate inventory when viewing/modifying cart

--- src/modules/cart/dtos/cart.response.dto.ts
+++ src/modules/cart/dtos/cart.response.dto.ts
@@ -4,8 +4,12 @@ export class CartResponseDto {
   id!: string;
   userId!: string;
   items!: CartItemResponseDto[];
+  hasUnavailableItems!: boolean;
 
   constructor(partial: Partial<CartResponseDto>) {
     Object.assign(this, partial);
+    this.hasUnavailableItems = (this.items ?? []).some(
+      (item) => !item.isAvailable,
+    );
   }
 }
