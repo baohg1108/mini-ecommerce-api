81585ad52e74aade67fd8ced8422759c3a8b5734
Giabao11082005 <hoanggiabao110820055@gmail.com>
2026-09-04 23:31:12 +0700

    fix(user): fix validate length for email, matches for phone and url for avatar

--- src/modules/users/dtos/update-user.dto.ts
+++ src/modules/users/dtos/update-user.dto.ts
@@ -4,11 +4,14 @@ import {
   IsOptional,
   MinLength,
   MaxLength,
+  Matches,
+  IsUrl,
 } from 'class-validator';
 
 export class UpdateUserDto {
   @IsOptional()
   @IsEmail()
+  @MaxLength(255)
   email?: string;
 
   @IsOptional()
@@ -26,11 +29,13 @@ export class UpdateUserDto {
   @IsString()
   @MinLength(8)
   @MaxLength(20)
+  @Matches(/^[0-9+\-\s()]{8,20}$/, { message: 'Phone number is invalid' })
   phone?: string;
 
   @IsOptional()
   @IsString()
   @MinLength(2)
   @MaxLength(255)
+  @IsUrl()
   avatarUrl?: string;
 }
