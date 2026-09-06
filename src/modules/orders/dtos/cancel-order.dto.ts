1860d5e7b78a0962149e7c782bc3879e4a8ebb13
Giabao11082005 <hoanggiabao110820055@gmail.com>
2026-09-06 16:16:38 +0700

    fix(order): BUG-004-SP7 - add validate length shippingAddress, createOrder and cancelOrder

--- src/modules/orders/dtos/cancel-order.dto.ts
+++ src/modules/orders/dtos/cancel-order.dto.ts
@@ -1,8 +1,9 @@
-import { IsOptional, IsString, MaxLength } from 'class-validator';
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CancelOrderDto {
   @IsOptional()
   @IsString()
+  @MinLength(10)
   @MaxLength(500)
   reason?: string;
 }
